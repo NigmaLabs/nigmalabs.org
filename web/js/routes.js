8fd6061eb1bddfc8bc132a3a24abff4fba7627e1
sebastianpozoga <prog255@gmail.com>
2014-11-02 00:18:49 +0100

    remove test

--- web/js/routes.js
+++ web/js/routes.js
@@ -1,55 +1,56 @@
-define(['./app'], function(app) {
+define(['./app'], function (app) {
     'use strict';
-    return app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-            
-            /*$routeProvider.when('/', {
-                templateUrl: 'partials/deklaracja.html'
-            });
-            
-            $routeProvider.when('/o-nas', {
-                templateUrl: 'partials/o-nas.html'
-            });*/
+    return app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
 
-            $routeProvider.when('/historia', {
-                templateUrl: 'partials/historia.html'
-            });
+        /*$routeProvider.when('/', {
+         templateUrl: 'partials/deklaracja.html'
+         });
 
-            $routeProvider.when('/statut', {
-                templateUrl: 'partials/statut.html'
-            });
+         $routeProvider.when('/o-nas', {
+         templateUrl: 'partials/o-nas.html'
+         });
 
-            $routeProvider.when('/deklaracja', {
-                templateUrl: 'partials/deklaracja.html'
-            });
+         $routeProvider.when('/historia', {
+         templateUrl: 'partials/historia.html'
+         });
 
-            $routeProvider.when('/zarzad', {
-                templateUrl: 'partials/zarzad.html'
-            });
+         $routeProvider.when('/statut', {
+         templateUrl: 'partials/statut.html'
+         });
 
-            $routeProvider.when('/finanse', {
-                templateUrl: 'partials/finanse.html',
-                controller: 'finance'
-            });
+         $routeProvider.when('/deklaracja', {
+         templateUrl: 'partials/deklaracja.html'
+         });
 
-            $routeProvider.when('/FAQ', {
-                templateUrl: 'partials/FAQ.html'
-            });
+         $routeProvider.when('/zarzad', {
+         templateUrl: 'partials/zarzad.html'
+         });
 
-            $routeProvider.when('/:pageName', {
-                template: '<div ng-bind-html="content"></div>',
-                controller: 'content'
-            });
+         $routeProvider.when('/FAQ', {
+         templateUrl: 'partials/FAQ.html'
+         });
+         */
 
-            $routeProvider.when('/', {
-                template: '<div ng-bind-html="content"></div>',
-                controller: 'content'
-            });
+        $routeProvider.when('/finanse', {
+            templateUrl: 'partials/finanse.html',
+            controller: 'finance'
+        });
 
-            $routeProvider.otherwise({
-                redirectTo: '/'
-            });
+        $routeProvider.when('/:pageName', {
+            template: '<div ng-bind-html="content"></div>',
+            controller: 'content'
+        });
 
-            // use the HTML5 History API
-            $locationProvider.html5Mode(true);
-        }]);
+        $routeProvider.when('/', {
+            template: '<div ng-bind-html="content"></div>',
+            controller: 'content'
+        });
+
+        $routeProvider.otherwise({
+            redirectTo: '/'
+        });
+
+        // use the HTML5 History API
+        $locationProvider.html5Mode(true);
+    }]);
 });
